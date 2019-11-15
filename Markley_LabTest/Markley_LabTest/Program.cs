6da264653d257848c39d35d58c24aed4487cf9f2
kmarkley-ksu <kmarkley@students.kennesaw.edu>
2019-11-14 21:17:40 -0500

    Made the project.

---
+++ Markley_LabTest/Markley_LabTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Markley_LabTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
